d845bc9ff62289f3417b1a80993b57827be8f1ff
alfianlion <alfianlion@gmail.com>
2022-06-13 14:02:04 +0800

    Working on fragmentation for navigation

--- app/src/main/java/sg/edu/np/mad/myapplication/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/myapplication/MainActivity.java
@@ -10,17 +10,51 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record);
+        setContentView(R.layout.activity_main);
 
-//        //To use the dropdown view in the Activity Start xml
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(null);
+        bottomNavigationView.setSelectedItemId(R.id.catalog);
+
+//        ***** To use the dropdown view in the Activity Start xml *****
 //        Spinner dropdownMenu = findViewById(R.id.exerciseDropdown); //Define object
 //        ArrayAdapter<CharSequence>spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.exerciseTypes, R.layout.activity_start);
 //        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 //        dropdownMenu.setAdapter(spinnerAdapter);
+
     }
+
+// ****** Fragment for bottom Navigation - Hui Xin ******
+//    FirstFragment firstFragment = new FirstFragment();
+//    SecondFragment secondFragment = new SecondFragment();
+//    ThirdFragment thirdFragment = new ThirdFragment();
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//        switch (item.getItemId()) {
+//            case R.id.person:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+//                return true;
+//
+//            case R.id.home:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
+//                return true;
+//
+//            case R.id.settings:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+//                return true;
+//        }
+//        return false;
+//    }
 }
