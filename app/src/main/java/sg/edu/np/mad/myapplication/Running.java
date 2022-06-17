e021676fd2489b9c5be00c5f56343957a41a9d13
OngWeiLun <S10222794@connect.np.edu.sg>
2022-06-17 17:59:21 +0800

    Redid Running and Workout classes, extending to Abstract class Exercise;

---
+++ app/src/main/java/sg/edu/np/mad/myapplication/Running.java
@@ -0,0 +1,20 @@
+package sg.edu.np.mad.myapplication;
+
+import java.util.Date;
+
+public class Running extends Exercise{
+    double Distance;
+    //Distance
+    public void setDistance(double dist){
+        Distance = dist;
+    }
+    public double getDistance(){
+        return Distance;
+    }
+
+    public Running(String title,Integer time, Date date, Integer id, double dist,String type){
+        super(title,time,date,id,type);
+        Distance = dist;
+    }
+
+}
