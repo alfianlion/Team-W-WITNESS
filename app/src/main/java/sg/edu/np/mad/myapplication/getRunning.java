48ef374f86218945a98ff58bfc7527ce1722589a
Klot-Rap <s10222829@connect.np.edu.sg>
2022-06-18 12:43:01 +0800

    Successful writing to DB based on user input

--- app/src/main/java/sg/edu/np/mad/myapplication/getRunning.java
+++ app/src/main/java/sg/edu/np/mad/myapplication/getRunning.java
@@ -10,17 +10,83 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class getRunning extends AppCompatActivity {
+
+    //1. Variables (store etInput & firebase with reference)
+    EditText workoutTitle, distanceTravelled, timeTaken;
+    Button addRunningBtn;
+
+    Date date;
+    String type; int id;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_running);
 
+        //2. Hooks to all xml elements in activity_get_running.xml
+        //2.1 Hooks to all userInput
+        workoutTitle = findViewById(R.id.workoutTitleInput);
+        distanceTravelled = findViewById(R.id.distanceTravelledInput);
+        timeTaken = findViewById(R.id.timeTakenInput);
 
+        //2.2 Hooks to Button (to listen to onClick listener)
+        addRunningBtn = findViewById(R.id.addRunningInputButton);
+
+        //3. "add" button OnClickListener
+        addRunningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //4. Initialise and get reference to Running node
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference("Exercises/Runnings/");
+
+                //5. Retrieve user inputs
+                // Convert EditText to String
+                String woTitle_string = workoutTitle.getText().toString();
+                String distanceTravelled_string = distanceTravelled.getText().toString();
+                String timeTaken_string = timeTaken.getText().toString();
+
+                // Convert to dataType for Firebase Database
+                int distanceTravelled_final = Integer.parseInt(distanceTravelled_string);
+                int timeTaken_final = Integer.parseInt(timeTaken_string);
+                double distanceTravelled_double = (double) distanceTravelled_final;
+
+                //Default user input <needs to be edit-ed to assign real Id and real date-time>
+                id = 1;
+                type = "Running";
+                date = new Date();
+
+
+                /* This function > Results in error
+                    //Get date
+                    long millis=System.currentTimeMillis();
+
+                    // creating a new object of the class Date
+                    java.sql.Date date = new java.sql.Date(millis);
+                */
+
+                //6. Create and Store running obj in local DB
+                Running runningObj = new Running(woTitle_string, timeTaken_final, date,id, distanceTravelled_double, type);
+
+                //7. Pass in Running Obj into Firebase with Id nesting(as identifier)
+                myRef.child("1115777").setValue(runningObj);
+                finish();
+            }
+        });
+
+
+        //"back" button OnClickListener
         Button back = findViewById(R.id.backButton);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,42 +94,5 @@ public class getRunning extends AppCompatActivity {
                 finish();
             }
         });
-
-
-        Button add = findViewById(R.id.addRunningInputButton);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText workoutTitle;
-                EditText distanceTravelled;
-                EditText timeTaken;
-
-                int id = 0;
-                String type = "Running";
-
-                workoutTitle = (EditText) findViewById(R.id.workoutTitleInput);
-                distanceTravelled = (EditText) findViewById(R.id.distanceTravelledInput);
-                timeTaken = (EditText) findViewById(R.id.timeTakenInput);
-
-                String timeTaken_string = timeTaken.getText().toString();
-                String distanceTravelled_string = distanceTravelled.getText().toString();
-
-                int distanceTravelled_final = Integer.parseInt(distanceTravelled_string);
-                int timeTaken_final = Integer.parseInt(timeTaken_string);
-                double distanceTravelled_double = distanceTravelled_final;
-                //Get date
-                long millis=System.currentTimeMillis();
-
-                // creating a new object of the class Date
-                java.sql.Date date = new java.sql.Date(millis);
-
-                Running r = new Running(workoutTitle.getText().toString(), timeTaken_final, date,id, distanceTravelled_double, type);
-
-
-                finish();
-            }
-        }
-
-        );
     }
 }
--- app/src/main/java/sg/edu/np/mad/myapplication/getWorkout.java
+++ app/src/main/java/sg/edu/np/mad/myapplication/getWorkout.java
@@ -12,62 +12,90 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class getWorkout extends AppCompatActivity {
 
-    Date dateObject;
+
+    //1. Variables (store etInput & firebase with reference)
+    EditText workoutTitle, numReps, numSets, timeTaken;
+    Button addWorkoutBtn;
+
+    Date date;
+    String type; int id;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_workout);
-        Button back = findViewById(R.id.backButton);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
 
-        Button add = findViewById(R.id.addWorkoutInputButton);
-        add.setOnClickListener(new View.OnClickListener() {
+        //2. Hooks to all xml elements in activity_get_workout.xml
+        //2.1 Hooks to all userInput
+        workoutTitle = findViewById(R.id.workoutTitleInput);
+        numReps = findViewById(R.id.numRepsInput);
+        numSets = findViewById(R.id.numSetsInput);
+        timeTaken = findViewById(R.id.timeTakenInput);
+
+        //2.2 Hooks to Button (to listen to onClick listener)
+        addWorkoutBtn = findViewById(R.id.addWorkoutInputButton);
+
+        //3. "add" button OnClickListener
+        addWorkoutBtn.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-                //Defines Variables
-                EditText workoutTitle;
-                EditText numReps;
-                EditText numSets;
-                EditText timeTaken;
 
-                int id = 0;
-                String type = "Workout";
+                //4. Initialise and get reference to Workout node
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference("Exercises/Workouts/");
 
-                // Access User input
-                workoutTitle = (EditText) findViewById(R.id.workoutTitleInput);
-
-                numReps = (EditText) findViewById(R.id.numRepsInput);
-                numSets = (EditText) findViewById(R.id.numSetsInput);
-                timeTaken = (EditText) findViewById(R.id.timeTakenInput);
-
+                //5. Retrieve user inputs
                 // Convert EditText to String
+                String woTitle_string = workoutTitle.getText().toString();
                 String numReps_string = numReps.getText().toString();
                 String numSets_string = numSets.getText().toString();
                 String timeTaken_string = timeTaken.getText().toString();
 
-                // Convert to dataType for Database
+                // Convert to dataType for Firebase Database
                 int numReps_final = Integer.parseInt(numReps_string);
                 int numSets_final = Integer.parseInt(numSets_string);
                 int timeTaken_final = Integer.parseInt(timeTaken_string);
 
-                //Get date
-                long millis=System.currentTimeMillis();
+                //Default user input <needs to be edit-ed to assign real Id and real date-time>
+                id = 1;
+                type = "Workout";
+                date = new Date();
 
-                // creating a new object of the class Date
-                java.sql.Date date = new java.sql.Date(millis);
 
-                Workout w = new Workout(workoutTitle.toString(),timeTaken_final,date,id,numReps_final,numSets_final,type);
+                /* This function > Results in error
+                    //Get date
+                    long millis=System.currentTimeMillis();
+
+                    // creating a new object of the class Date
+                    java.sql.Date date = new java.sql.Date(millis);
+                */
+
+                //6. Create and Store workout obj in local DB
+                Workout workoutObj = new Workout(woTitle_string,timeTaken_final,date,id,numReps_final,numSets_final,type);
+
+                //7. Pass in Workout Obj into Firebase with Id nesting(as identifier)
+                myRef.child("7772333").setValue(workoutObj);
+                finish();
+            }
+        });
+
+        //"back" button OnClickListener
+        Button back = findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 finish();
             }
         });
