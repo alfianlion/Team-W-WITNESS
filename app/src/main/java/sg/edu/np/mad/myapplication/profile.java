244a49dfb551448d58daf1befeb2f94559023603
alfianlion <alfianlion@gmail.com>
2022-06-18 11:12:31 +0800

    Fragment issue

--- app/src/main/java/sg/edu/np/mad/myapplication/profile.java
+++ app/src/main/java/sg/edu/np/mad/myapplication/profile.java
@@ -3,6 +3,7 @@ package sg.edu.np.mad.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -10,7 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -50,6 +59,12 @@ public class profile extends Fragment {
         return fragment;
     }
 
+    private FirebaseAuth mAuth;
+    EditText username,password;
+    View view;
+    Button register, login;
+    ValueEventListener help;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,11 +72,19 @@ public class profile extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        username = (EditText) findViewById(R.id.usernameInput);
+        password = (EditText) findViewById(R.id.passwordInput);
+
+
+        login = (Button) view.findViewById(R.id.loginBtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin(username.getText().toString(),password.getText().toString());
+            }
+        });
     }
-
-
-    View view;
-    Button register;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,6 +92,17 @@ public class profile extends Fragment {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.profile_login, container, false);
         view = inflater.inflate(R.layout.profile_login, container, false);
+        username = (EditText) view.findViewById(R.id.usernameInput);
+        password = (EditText) view.findViewById(R.id.passwordInput);
+
+        login = (Button) view.findViewById(R.id.loginBtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin(username.getText().toString(),password.getText().toString());
+            }
+        });
+
         register = (Button) view.findViewById(R.id.registerIntentBtn);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,5 +112,36 @@ public class profile extends Fragment {
             }
         });
         return view;
+    }
+
+    private void userLogin(String u, String p){
+        String username_string = u;
+        String password_string = p;
+
+
+        if(username_string.isEmpty()){
+            username.setError("Name is required");
+            username.requestFocus();
+            return;
+        }
+
+        if(password_string.isEmpty()){
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(username_string, password_string).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getContext(), "Login Successful",Toast.LENGTH_LONG).show();
+                    Log.i("Login","yourmom");
+                    startActivity(new Intent(getActivity(),MainActivity.class));
+                } else {
+                    Toast.makeText(getContext(), "Failed to login! Please check your credentials",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
