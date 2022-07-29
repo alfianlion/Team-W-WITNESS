a04d7c4c42b7c5a071e74c2e4392919119046c83
HuiXin681 <s10223047@connect.np.edu.sg>
2022-07-30 06:35:55 +0800

    made layout nicer

--- app/src/main/java/sg/edu/np/mad/WittnessFittness/weatherChecker.java
+++ app/src/main/java/sg/edu/np/mad/WittnessFittness/weatherChecker.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,11 +29,11 @@ import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 
-public class weatherChecker extends AppCompatActivity implements LocationListener{
+public class weatherChecker extends AppCompatActivity implements LocationListener {
     protected LocationManager locationManager;
     protected LocationListener locationListener;
     protected Context context;
-    TextView showLon;
+    TextView Loading;
     TextView showLat;
     ImageView icon;
 
@@ -59,65 +58,33 @@ public class weatherChecker extends AppCompatActivity implements LocationListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_checker);
 
-        showLon = (TextView) findViewById(R.id.displayLon);
+        Loading = (TextView) findViewById(R.id.loading);
 
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 
     @Override
-    public void onLocationChanged (Location location){
-        showLon = (TextView) findViewById(R.id.displayLon);
-        String lan = Long.toString(Math.round((location.getLatitude())));
-        String lon = Long.toString(Math.round((location.getLongitude())));
-        showLon.setText(lon);
-        showLat = (TextView) findViewById(R.id.displayLat);
-        showLat.setText(lan);}
-    @Override
-    public void onProviderDisabled (String provider){
-        Log.d("Latitude", "disable");
-    }
-
-    @Override
-    public void onProviderEnabled (String provider){
-        Log.d("Latitude", "enable");
-    }
-
-    @Override
-    public void onStatusChanged (String provider,int status, Bundle extras){
-        Log.d("Latitude", "status");
-    }
-
-    public void getWeather(View view){
-        TextView WeatherStatus = findViewById(R.id.weatherStatus);
-        TextView weatherDesc = findViewById(R.id.weatherDescription);
-        TextView temperature = findViewById(R.id.temperature);
-        TextView humidity = findViewById(R.id.humidity);
-        TextView getLon = findViewById(R.id.displayLon);
-        ImageView icon = findViewById(R.id.weatherIcon);
-        String lon = getLon.getText().toString();
-        TextView getLat = findViewById(R.id.displayLat);
-        String lat = getLat.getText().toString();
-        Log.d("getWeather:", lat);
+    public void onLocationChanged(Location location) {
         String tempUrl = "";
-
-        tempUrl = url + "?lat=" + lat + "&lon=" + lon + "&appid=" + appId;
+        tempUrl = url + "?lat=" + Math.round(location.getLatitude()) + "&lon=" + Math.round(location.getLongitude())  + "&appid=" + appId;
         StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Log.d("response", response);
                 String output = "";
                 Log.d("response", response);
-                try{
+                try {
                     JSONObject jsonResponse = new JSONObject(response);
                     JSONArray jsonArray = jsonResponse.getJSONArray("weather");
                     JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
@@ -125,31 +92,36 @@ public class weatherChecker extends AppCompatActivity implements LocationListene
                     String description = jsonObjectWeather.getString("description");
                     JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
                     double temp = jsonObjectMain.getDouble("temp") - 273.15;
-                    int humid= jsonObjectMain.getInt("humidity");
+                    int humid = jsonObjectMain.getInt("humidity");
                     Log.d("weather", main);
+                    Loading.setVisibility(View.GONE);
+                    TextView WeatherStatus = findViewById(R.id.weatherStatus);
+                    TextView weatherDesc = findViewById(R.id.weatherDescription);
+                    TextView temperature = findViewById(R.id.temperature);
+                    TextView humidity = findViewById(R.id.humidity);
+                    ImageView icon = findViewById(R.id.weatherIcon);
                     WeatherStatus.setVisibility(View.VISIBLE);
                     weatherDesc.setVisibility(View.VISIBLE);
                     temperature.setVisibility(View.VISIBLE);
                     humidity.setVisibility(View.VISIBLE);
                     icon.setVisibility(View.VISIBLE);
 
-                    WeatherStatus.setText(main+"");
-                    weatherDesc.setText(description+"");
-                    temperature.setText(df.format(temp) + " Celcuis");
-                    humidity.setText(humid+" %");
+                    WeatherStatus.setText(main + "");
+                    weatherDesc.setText(description + "");
+                    temperature.setText("Temperature: "+df.format(temp) + " Celcuis");
+                    humidity.setText("Humidity: "+ humid + " %");
 
-                    if (main == "Clear"){
-                     icon.setBackgroundResource(R.drawable.clearsky);
-                    }
-                    else if (main == "Cloudy"){
-                        icon.setBackgroundResource(R.drawable.cloudy);
-                    }
-                    else if (main == "Thunderstorm"){
-                        icon.setBackgroundResource(R.drawable.thunderstorm);
-                    }
-                    else if(main == "")
+                    if (main == "Clear") {
+                        icon.setImageDrawable(getResources().getDrawable(R.drawable.clearsky));
+                    } else if (main == "Clouds") {
+                        icon.setImageDrawable(getResources().getDrawable(R.drawable.cloudy));
+                    } else if (main == "Thunderstorm") {
+                        icon.setImageDrawable(getResources().getDrawable(R.drawable.thunderstorm));
+                    } else if (main == "") {
 
-                  } catch (JSONException e) {
+                    }
+
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
@@ -162,5 +134,19 @@ public class weatherChecker extends AppCompatActivity implements LocationListene
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
     }
-}
 
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude", "disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
+    }
+}
